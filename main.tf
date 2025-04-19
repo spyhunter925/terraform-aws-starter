cc7b85383393e46d3fdff83d50b2d2f76d6250cc
spyhunter925 <au@amescodrug.com>
2025-04-19 21:10:18 +0800

    modified for persisent security groups

--- main.tf
+++ main.tf
@@ -1,49 +1,42 @@
-terraform {
-  cloud {
-    organization = "your-org-name"
-
-    workspaces {
-      name = "aws-test-ec2"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "starter-vpc"
-  }
 }
 
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}a"
-
-  tags = {
-    Name = "starter-subnet"
-  }
+  availability_zone = "ap-southeast-1a"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "ubuntu_base" {
+  name        = "ubuntu-base-sg"
+  description = "Allow SSH and HTTP(S) access"
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -54,13 +47,17 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "ubuntu-base-sg"
+  }
 }
 
 resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  ami                         = var.ami_id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.main.id
+  vpc_security_group_ids      = [aws_security_group.ubuntu_base.id]
 
   tags = {
     Name = "TerraformEC2"
--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable "aws_region" {
-  description = "AWS region to deploy to"
-  type        = string
-  default     = "ap-southeast-1"
-}
-
 variable "ami_id" {
   description = "AMI ID to use for the EC2 instance"
   type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-1"
 }
